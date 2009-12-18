e3644dc0e10988a9f73b47c93b3c8f3a00f5925a
Zhu Jia Cheng <realalien@gmail.com>
2009-12-18 18:45:20 +0800

    begin scripting

---
+++ src/p42ab.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+
+
+
+
+
+
+
+# ESP. COMMMANDS
+
+# log on
+ab logon -u Administrator -p "mes0Spicy" -d p4mirror -s Spicyfile
+
+# add file
+ab 
+
+
+
+#Usage Examples:
+#---------------
+#ab help checkout
+#ab h gl
+#ab enumprojects -s NXNSERVER
+#ab logon -u John -p "" -d Demo_Project -s NXNSERVER
+#ab setworkingpath "c:\myworkingpath\Demo_Project"
+#ab enumobjects
+#ab isuptodate picture1.bmp
+#ab find -checkedoutby "John"
+#ab checkout picture1.bmp -comment "Modifying background" -response:CheckOut.Writ
+#able y
+#ab logoff
+
+
+
+
